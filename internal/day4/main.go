14212c1b4e81d46889ce727be316773350a2b762
SalzstangeSalamiBrief <jeffrey.pluntke@googlemail.com>
2024-12-21 10:40:22 +0000

    Add vertical checks; Refactor

--- internal/day4/main.go
+++ internal/day4/main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"salzstangeSalamiBrief/adventOfCode24/pkg/fileReader"
-	"strings"
 )
 
 const fileName string = "exampleInput.txt"
 
 var wordParts []string = []string{"X", "M", "A", "S"}
-var horizontalJoinedWord = strings.Join(wordParts, "")
 
 func main() {
 	multidimensionalCharacterArrayInput := fileReader.GetFileContentAsMultidimensionalCharacterArray(fileName)
 	indexesOfStartingCharacterX := getIndexesOfStartingCharacterX(multidimensionalCharacterArrayInput)
+
+	sum := 0
+	for _, tupleOfIndexes := range indexesOfStartingCharacterX{
+		sum += getNumberOfMatchesForStartingCharacter(multidimensionalCharacterArrayInput, tupleOfIndexes)
+	}
+	fmt.Printf("sum '%v'", sum)
 }
 
 func getIndexesOfStartingCharacterX(lines [][]string) [][2]int {
@@ -21,42 +26,93 @@ func getIndexesOfStartingCharacterX(lines [][]string) [][2]int {
 		for j, character := range line {
 			if character == "X" {
 				indexes = append(indexes, [2]int{i, j})
-			}
+			}}}
+		return indexes}
+
+func getNumberOfMatchesForStartingCharacter(puzzleInput [][]string, startingIndex [2]int) int {
+	isHorizontalLeftMatching := getIsHorizontalLeftMatching(puzzleInput[startingIndex[0]], startingIndex[1])
+	isHorizontalRightMatching := getIsHorizontalRightMatching(puzzleInput[startingIndex[0]], startingIndex[1])
+	isVerticalTopMatching := getIsVerticalTopMatching(puzzleInput, startingIndex[0], startingIndex[1])
+	isVerticalBottomMatching := getIsVerticalBottomMatching(puzzleInput, startingIndex[0], startingIndex[1])
+
+	fmt.Printf("[row, col]: '[%v, %v]'\n", startingIndex[0], startingIndex[1])
+	fmt.Printf("left '%v', right '%v', top '%v', bottom '%v'\n", isHorizontalLeftMatching, isHorizontalRightMatching, isVerticalTopMatching, isVerticalBottomMatching)
+	return isHorizontalLeftMatching + isHorizontalRightMatching+isVerticalTopMatching+isVerticalBottomMatching
+}
+
+func getIsHorizontalRightMatching(line []string, col int) int {
+	if getIsHorizontalRightOutOfBound(line, col){
+		return 0
+	}
+
+	for i, part := range wordParts {
+		isCharacterMatching := line[col+i] == part
+		if !isCharacterMatching{
+			return 0;
 		}
 	}
 
-	return indexes
+	return 1
 }
 
-func getNumberOfMatchesForStartingCharacter(line []string, startingIndex [2]int) int {
-	isHorizontalLeftMatching := getIsHorizontalLeftMatching(line, startingIndex[0])
-	isHorizontalRightMatching := getIsHorizontalRightMatching(line, startingIndex[0])
-}
-
-func getIsHorizontalRightMatching(line []string, col int) bool {
-	if col > len(line)-len(wordParts) {
-		return false
+func getIsHorizontalLeftMatching(line []string, col int) int {
+	if getIsHorizontalLeftOutOfBound(col) {
+		return 0
 	}
 
-	isWordMatching := true
-	for i, part := range wordParts {
-		isCharacterMatching := line[col+i] == part
-		isWordMatching = isWordMatching && isCharacterMatching
-	}
-
-	return isWordMatching
-}
-
-func getIsHorizontalLeftMatching(line []string, col int) bool {
-	if col-len(wordParts)-1 < 0 {
-		return false
-	}
-
-	isWordMatching := true
 	for i, part := range wordParts {
 		isCharacterMatching := line[col-i] == part
-		isWordMatching = isWordMatching && isCharacterMatching
+		if !isCharacterMatching{
+			return 0
+		}
 	}
 
-	return isWordMatching
+	return 1
+}
+
+func getIsVerticalBottomMatching(puzzleInput [][]string, row int, col int) int{
+	if getIsVerticalBottomOutOfBound(puzzleInput, row){
+		return 0
+	}
+
+		for i, part := range wordParts{
+			isCharacterMatching := puzzleInput[row+i][col] == part
+			if !isCharacterMatching{
+				return 0
+			}		
+	}
+
+	return 1
+}
+
+func getIsVerticalTopMatching(puzzleInput [][]string, row int, col int) int{
+	if getIsVerticalTopOutOfBound(row){
+		return 0
+	}
+
+		for i, part := range wordParts{
+			isCharacterMatching := puzzleInput[row-i][col] == part
+			if !isCharacterMatching{
+				return 0
+			}		
+	}
+
+	return 1
+}
+
+
+func getIsHorizontalLeftOutOfBound(col int) bool{
+	return col - len(wordParts) <= 0
+}
+
+func getIsHorizontalRightOutOfBound(line []string, col int) bool{
+	return col + len(wordParts) >= len(line)
+}
+
+func getIsVerticalBottomOutOfBound(puzzleInput [][]string, row int) bool{
+	return row + len(wordParts) >= len(puzzleInput)
+}
+
+func getIsVerticalTopOutOfBound(row int) bool{
+	return row - len(wordParts) <= 0
 }
